829e19e0d38a670c122b89e5681f74c49532b993
MaxraySavage <maxraysavage@gmail.com>
2022-01-14 16:06:27 -0500

    add spring intializr generated files

---
+++ src/main/java/org/launchcode/VennTime/VennTimeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.VennTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VennTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VennTimeApplication.class, args);
+	}
+
+}
