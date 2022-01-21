687c4f165a7c61237c02b52be806f9643db50e3b
MaxraySavage <maxraysavage@gmail.com>
2022-01-20 20:34:40 -0500

    adds code to Process form submit

--- src/main/java/org/launchcode/VennTime/controllers/HomeController.java
+++ src/main/java/org/launchcode/VennTime/controllers/HomeController.java
@@ -1,15 +1,31 @@
 package org.launchcode.VennTime.controllers;
 
+import org.launchcode.VennTime.data.EventRepository;
+import org.launchcode.VennTime.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    EventRepository eventRepository;
+
     @GetMapping("/")
     public String getHomePage(Model model) {
         model.addAttribute("event", new Event());
         return "home";
+    }
+
+    @ResponseBody
+    @PostMapping("/")
+    public String processCreateEvent(Model model, @ModelAttribute Event newEvent){
+        eventRepository.save(newEvent);
+        return newEvent.getName();
     }
 }
