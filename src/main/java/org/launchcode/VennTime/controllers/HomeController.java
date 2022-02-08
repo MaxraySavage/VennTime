627b938eadfe366fa57290360e7d70beb6ee908f
MaxraySavage <maxraysavage@gmail.com>
2022-02-07 19:07:26 -0500

    remove date field from dto

--- src/main/java/org/launchcode/VennTime/controllers/HomeController.java
+++ src/main/java/org/launchcode/VennTime/controllers/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,7 +30,6 @@ public class HomeController {
     }
 
 
-    @ResponseBody
     @PostMapping("/")
     public String processCreateEvent(Model model, @ModelAttribute @Valid CreateEventDTO createEventDTO, Errors errors) throws ParseException {
 
